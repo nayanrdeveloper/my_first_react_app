6f3adf9fa0589e98343344aebc5f0a1b3bef7a06
Nayan <nayanrdeveloper@gmail.com>
2022-04-18 07:46:24 +0530

    [Add add code and first learn how to use JSX in React]

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
